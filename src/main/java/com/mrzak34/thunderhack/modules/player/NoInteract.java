b9beb52345bdf6b75d03274d504741d24387551e
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2023-02-05 23:38:28 +0300

    Update NoInteract.java

--- src/main/java/com/mrzak34/thunderhack/modules/player/NoInteract.java
+++ src/main/java/com/mrzak34/thunderhack/modules/player/NoInteract.java
@@ -2,11 +2,10 @@ package com.mrzak34.thunderhack.modules.player;
 
 import com.mrzak34.thunderhack.events.PacketEvent;
 import com.mrzak34.thunderhack.modules.Module;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemBow;
-import net.minecraft.item.ItemSword;
-import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
-import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.SPacketOpenWindow;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class NoInteract extends Module {
@@ -15,19 +14,29 @@ public class NoInteract extends Module {
     }
 
 
+
     @SubscribeEvent
-    public void onPacketSend(PacketEvent.Send event){
-        if(fullNullCheck()) return;
-        if(mc.player.getHeldItemMainhand().getItem() instanceof ItemSword || mc.player.getHeldItemMainhand().getItem() instanceof ItemAxe ||  mc.player.getHeldItemMainhand().getItem() instanceof ItemBow) {
-            if (event.getPacket() instanceof CPacketPlayerTryUseItemOnBlock) {
-                event.setCanceled(true);
-            }
-            if (event.getPacket() instanceof CPacketUseEntity) {
-                CPacketUseEntity var2 = (CPacketUseEntity) event.getPacket();
-                if (var2.getAction() == CPacketUseEntity.Action.INTERACT || var2.getAction() == CPacketUseEntity.Action.INTERACT_AT) {
-                    event.setCanceled(true);
-                }
-            }
+    public void onInteract(PlayerInteractEvent.RightClickBlock event) {
+        if (fullNullCheck()) return;
+
+        Block block = mc.world.getBlockState(event.getPos()).getBlock();
+        if (block == Blocks.ANVIL
+                || block == Blocks.ENDER_CHEST
+                || block == Blocks.CRAFTING_TABLE
+                || block == Blocks.ACACIA_FENCE_GATE
+                || block == Blocks.BIRCH_FENCE_GATE
+                || block == Blocks.DARK_OAK_FENCE_GATE
+                || block == Blocks.JUNGLE_FENCE_GATE
+                || block == Blocks.SPRUCE_FENCE_GATE
+                || block == Blocks.OAK_FENCE_GATE) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onPacketReceive(PacketEvent.Receive e){
+        if(e.getPacket() instanceof SPacketOpenWindow && !fullNullCheck() && (!mc.player.getHeldItemMainhand().isEmpty() || !mc.player.getHeldItemOffhand().isEmpty())){
+            e.setCanceled(true);
         }
     }
 }
